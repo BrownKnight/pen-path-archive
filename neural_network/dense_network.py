c6a7fbd3c1e64517cb4fa2127ef5be1a83070268
BrownKnight <amanrk@hotmail.co.uk>
2020-03-08 19:13:11 +0000

    Add convolutional and dense models for experimentation

--- neural_network/dense_network.py
+++ neural_network/dense_network.py
@@ -2,30 +2,30 @@ import glob
 
 from tensorflow_core.python.keras import Model
 from tensorflow_core.python.keras.layers import Input, LSTM, RepeatVector, TimeDistributed, Dense, Masking, Embedding, \
-    Reshape
+    Reshape, Conv2D, MaxPooling2D, BatchNormalization, LeakyReLU, UpSampling2D
 from tensorflow.keras import models, losses, optimizers
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 # MODEL_PATH = "models/model_300_neurons_0.00001_lr_char-01-000-*-*.h5"
-MODEL_PATH = "models/autoencoder_lstm_functional.h5"
+
+MODEL_PATH = "models/dense_autoencoder.h5"
 TEST_SPLIT = 0.1
 
 
 def main():
-    # x_data = load_x("test.nosync/image_output/char-*-000-*-*.csv")
-    # normalize_x(x_data)
-    # target_data = load_y("test.nosync/ground_truth/char-*-000-*-*.txt")
-    # normalize_y(target_data)
-    # print(target_data)
-    #
-    # model = create_model()
-    # train_model(model, target_data, target_data)
+    x_data = load_y("test.nosync/image_output/char-*-000-*-*.csv")
+    normalize_y(x_data)
+    target_data = load_y("test.nosync/ground_truth/char-*-000-*-*.txt")
+    normalize_y(target_data)
 
-    # test_data = load_y("test.nosync/ground_truth/char-01-000-*-*.txt")
-    test_data = load_x("test.nosync/image_output/char-*-000-*-*.csv")
-    normalize_x(test_data)
+    model = create_model()
+    train_model(model, x_data, target_data)
+
+    # test_data = load_y("test.nosync/ground_truth/char-*-000-*-*.txt")
+    test_data = load_y("test.nosync/image_output/char-*-000-*-*.csv")
+    normalize_y(test_data)
     model = models.load_model(MODEL_PATH)
     print(model.summary())
     predict(model, test_data)
@@ -36,22 +36,27 @@ def load_y(path):
     file_paths = glob.glob(path)
     file_paths.sort()
     num_files = len(file_paths)
-    data = np.zeros((num_files, 128, 2))
+    data = np.zeros((num_files, 64, 64, 1))
 
     for index, file_path in enumerate(file_paths):
         with open(file_path) as file:
             lines = file.readlines()
-            char = np.asarray([np.asarray(point.split(",")) for point in lines])
+            lines = [np.asarray(point.split(',')[:2], dtype=int) for point in lines]
+            char = np.zeros((64, 64, 1))
+            for j, point in enumerate(lines):
+                if point[0] == 0 and point[1] == 0:
+                    continue
+                char[point[1], point[0], 0] = j + 1
             data[index] = char
 
     print("Loaded %s character target data files" % num_files)
 
-    np.random.shuffle(data)
+    # np.random.shuffle(data)
     return data
 
 
 def normalize_y(data):
-    data /= 64
+    data /= 128
 
 
 def load_x(path):
@@ -77,22 +82,19 @@ def normalize_x(data):
 
 
 def create_model():
-    encoder_inputs = Input(shape=(128, 2))
-    masked_encoder_inputs = Masking()(encoder_inputs)
-    encoder_lstm = LSTM(1024, return_state=True)
-    encoder_outputs, state_h, state_c = encoder_lstm(masked_encoder_inputs)
-    # We discard `encoder_outputs` and only keep the states.
-    encoder_states = [state_h, state_c]
-
-    decoder_inputs = Input(shape=(128, 2), )
-    masked_decoder_inputs = Masking()(decoder_inputs)
-    decoder_lstm = LSTM(1024, return_state=True, return_sequences=True)
-    decoder_outputs, _, _ = decoder_lstm(masked_decoder_inputs, initial_state=encoder_states)
-
-    outputs = TimeDistributed(Dense(2, activation='sigmoid'))(decoder_outputs)
-
-    model = Model([encoder_inputs, decoder_inputs], outputs)
-    model.compile(optimizer=optimizers.Adam(learning_rate=0.00003), loss=losses.MeanAbsoluteError(),
+    model = models.Sequential([
+        Input((64, 64, 1)),
+        Dense(128),
+        LeakyReLU(),
+        Dense(256),
+        LeakyReLU(),
+        Dense(256),
+        LeakyReLU(),
+        Dense(128),
+        LeakyReLU(),
+        Dense(1, activation='sigmoid'),
+    ])
+    model.compile(optimizer=optimizers.Adam(learning_rate=0.00001), loss=losses.MeanAbsoluteError(),
                   metrics=["accuracy"])
 
     print(model.summary())
@@ -102,7 +104,7 @@ def create_model():
 
 def train_model(model: models.Sequential, train_x: np.ndarray, train_y: np.ndarray):
     print("Training Model")
-    history = model.fit([train_x, train_x], train_y, batch_size=256, epochs=1000, verbose=1, validation_split=TEST_SPLIT,
+    history = model.fit(train_x, train_y, batch_size=32, epochs=150, verbose=1, validation_split=TEST_SPLIT,
                         shuffle=True)
 
     plt.plot(history.history['accuracy'], label='accuracy')
@@ -119,40 +121,25 @@ def train_model(model: models.Sequential, train_x: np.ndarray, train_y: np.ndarr
 
 
 def predict(model: models.Sequential, test_data: np.ndarray):
-    result = model.predict([test_data, test_data])
+    result = model.predict(test_data)
 
-    result = result[0] * 64
-    # result = result[0]
+    result = result[0] * 128
+    result = result.reshape((64, 64))
     print(result)
-    np.savetxt("test.nosync/result.txt", result)
-    result_image = create_image_from_data(result)
 
-    test = test_data[0] * 64
-    test_image = create_image_from_data(test)
+    test = test_data[0] * 128
+    test = test.reshape((64, 64))
+    # test_image = create_image_from_data(test)
 
     # write the result array to file
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
-    colorbar1 = ax1.imshow(test_image)
+    colorbar1 = ax1.imshow(test)
     fig.colorbar(colorbar1, ax=ax1)
-    colorbar2 = ax2.imshow(result_image)
+    colorbar2 = ax2.imshow(result)
     fig.colorbar(colorbar2, ax=ax2)
 
     plt.show()
-
-
-def create_image_from_data(data: np.ndarray):
-    image = np.zeros((64, 64), float)
-    for i, point in enumerate(data):
-        x = int(point[0])
-        y = int(point[1])
-
-        if x == 0 and y == 0:
-            continue
-
-        image[y, x] = i + 1
-
-    return image
 
 
 if __name__ == "__main__":
