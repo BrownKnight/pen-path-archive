0d201bc4d3549c344cfc8934325555cb3328820a
BrownKnight <amanrk@hotmail.co.uk>
2020-04-07 12:33:48 +0100

    fix imports

--- neural_network/create_test_data.py
+++ neural_network/create_test_data.py
@@ -1,12 +1,12 @@
-from neural_network import uji_encoder
+import neural_network.uji_encoder as uji_encoder
 
 print("Creating GT Files")
 uji_encoder.main()
 
 print("Processing image_input")
-from image_processing import main as image_processing
+import image_processing.main as image_processing
 
 image_processing.run("directory")
 
 print("Filtering out gt files")
-from neural_network import filter_gt_files
+import neural_network.filter_gt_files
