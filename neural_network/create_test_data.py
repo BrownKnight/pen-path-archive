1944a8bc37fb6ffa9bd938a8a17f183842b9f24e
BrownKnight <amanrk@hotmail.co.uk>
2020-04-07 12:31:52 +0100

    fix

--- neural_network/create_test_data.py
+++ neural_network/create_test_data.py
@@ -1,7 +1,7 @@
-from neural_network.uji_encoder import main as uji_encoder
+from neural_network import uji_encoder
 
 print("Creating GT Files")
-uji_encoder()
+uji_encoder.main()
 
 print("Processing image_input")
 from image_processing import main as image_processing
